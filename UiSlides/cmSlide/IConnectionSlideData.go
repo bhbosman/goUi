6ee9d10619107d87b465a237969b421f17683d32
Brendan Bosman <bhbosman@gmail.com>
2023-03-27 12:53:06 +0200

    Work In Progress

--- UiSlides/cmSlide/IConnectionSlideData.go
+++ UiSlides/cmSlide/IConnectionSlideData.go
@@ -1,6 +1,6 @@
 package cmSlide
 
-import "github.com/bhbosman/gocommon/Services/IDataShutDown"
+import "github.com/bhbosman/gocommon/services/IDataShutDown"
 
 type IConnectionSlideData interface {
 	IConnectionSlide
